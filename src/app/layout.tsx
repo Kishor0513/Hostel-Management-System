ad6b1d32afbd096001b9ce00f375dc909f23a334
Kishor <kishorc2000@gmail.com>
2026-03-23 16:01:01 +0545

    chore: update src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+
+import GlobalMouseFollow from '@/components/effects/GlobalMouseFollow';
+
 import './globals.css';
 
 const geistSans = Geist({
@@ -28,7 +31,10 @@ export default function RootLayout({
 			data-theme="light"
 			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
 		>
-			<body className="min-h-full flex flex-col bg-slate-950">{children}</body>
+			<body className="min-h-full flex flex-col bg-(--background)">
+				<GlobalMouseFollow />
+				{children}
+			</body>
 		</html>
 	);
 }
